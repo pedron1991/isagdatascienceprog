1265f6b71f344a001a298306f77b5b2801066463
Rafael Almeida <62503571+Rafadalmeida@users.noreply.github.com>
2020-11-13 20:03:12 +0000

    Criação ficeiro python

---
+++ trabalho.py
@@ -0,0 +1,12 @@
+#Pós graduação em Data science and BI
+#Trabalho de programação
+#Florbela Amaral
+#Pedro Fernandes
+#Rafael Almeida
+
+# K-MEANS
+
+import pandas as pd
+import numpy as np
+import matplotlib.pylot as plt
+from sklearn.cluster import KMeans
